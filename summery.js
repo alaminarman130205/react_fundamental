368ca408a2eb4681a18943fcef8d60cd5f782bae
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-13 21:48:41 +0600

    jante hobe ja ja

---
+++ summery.js
@@ -0,0 +1,32 @@
+/*
+1 variable let const three types of variable 
+2 condition gula jante hobe 
+3 and or agula jante hobe if  else and elseif else agula jante hobe 
+4 array declarae kora push pop index jante hobe include jante hobe indexOf 
+5 for loop while loop 
+6 function declare perameter jante hobe 
+7 object jante hobe object property jante hobe valuer man array soho object soho 
+**********es6 
+8 template string jante hobe 
+9 spread operator 
+10 filter find map agula jante hobe 
+11 arrow function 
+12 no perameter multiple perameter , 
+13 destructuring jante hobe array r modde destructuring jante hobe 
+14 browser api /// 
+1 localStorage 
+2 session storage 
+3 location api 
+4 history api 
+5 fetch 
+6 async await 
+7 json stringify parse agula jante hobe 
+
+other's  
+1 array ar modde map foreach find 
+shorthand 
+1 ternary operator 
+2 logical and and locagical or && || agula jante hobe 
+3 optional chaining jante hobe 
+
+*/
